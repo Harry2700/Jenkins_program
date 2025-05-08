fc6f7813c6572b563fb317fd5a57dc170f9ae4f3
Harry2700 <hariomdhiman99@gmail.com>
2025-05-08 20:50:47 +0530

    one more changes

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main{
         System.out.println("this is java program");
         System.out.println("this is output of java program");
         System.out.println(new Date());
-        System.out.println("Hello");
+
+        System.out.println("Hello world");
     }
 }
